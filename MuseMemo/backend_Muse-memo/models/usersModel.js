b78f7ca11115ef966267ff8db26e8d0f6c3b9abc
Fardin Ahsan <fardin2002@cseku.ac.bd>
2025-04-17 02:04:11 +0600

    MuseMemo: Backend Completed.

---
+++ MuseMemo/backend_Muse-memo/models/usersModel.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema
+
+const usersSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    number: {
+        type: String,
+        required: true,
+    },
+    dateOfBirth: {
+        type: Date,
+        required: true,
+    },
+    gender: {
+        type: String,
+        enum:["male", "female", "other"],
+        required: true,
+    },
+
+})
+
+
+export default mongoose.model('Users', usersSchema)
